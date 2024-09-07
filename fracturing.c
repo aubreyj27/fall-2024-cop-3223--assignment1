7db72f7b2019c7651d2a6ce28d5af5654442ea38
aubreyj27 <skyeyoung0723@gmail.com>
2024-09-07 13:40:10 +0000

    i removed the stars

--- fracturing.c
+++ fracturing.c
@@ -26,10 +26,11 @@
 // Return: 0
 //#########################################################
 
-void scanAndPrint(double *x1, double *x2, double *y1, double *y2)
+void scanAndPrint(double x1, double x2, double y1, double y2)
 {
     // scan and print x and y variables
     scanf("%lf%lf%lf%lf", &x1, &x2, &y1, &y2);
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
     printf("Point #1 entered: x1 = %lf ; y1 = %lf", x1, y1);
@@ -44,6 +45,10 @@ void scanAndPrint(double *x1, double *x2, double *y1, double *y2)
     printf("Point #1 entered: x1 = %.1lf ; y1 = %.1lf\n", *x1, *y1);
     printf("Point #2 entered: x2 = %.1lf ; y2 = %.1lf\n", *x2, *y2);
 >>>>>>> e9fba52 (aaaaaaaaaaaaaaaaaaaaaaaa)
+=======
+    printf("Point #1 entered: x1 = %.1lf ; y1 = %.1lf\n", x1, y1);
+    printf("Point #2 entered: x2 = %.1lf ; y2 = %.1lf\n", x2, y2);
+>>>>>>> 0a87b96 (i removed the stars)
 }
 
 //#########################################################
